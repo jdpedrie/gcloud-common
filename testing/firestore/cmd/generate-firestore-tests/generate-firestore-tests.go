a5e91da33c90bed7af7aaffda7f00dacfdd6c026
Jonathan Amsterdam <jba@google.com>
2018-02-13 15:39:33 -0500

    fix typo

--- testing/firestore/cmd/generate-firestore-tests/generate-firestore-tests.go
+++ testing/firestore/cmd/generate-firestore-tests/generate-firestore-tests.go
@@ -789,7 +789,7 @@ Each FieldPath is a sequence of uninterpreted path components.`,
 		{
 			suffix:  "fp-del", // see https://github.com/googleapis/nodejs-firestore/pull/119
 			desc:    "field paths with delete",
-			comment: `One one nested field is deleted, and another isn't, preserve the second.`,
+			comment: `If one nested field is deleted, and another isn't, preserve the second.`,
 			paths:   [][]string{{"foo", "bar"}, {"foo", "delete"}},
 			values:  []string{`1`, `"Delete"`},
 			outData: mp("foo", mp("bar", 1)),
